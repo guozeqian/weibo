5c3a6212221c89c26451167d95061ba8b776d000
guozeqian <787033483@qq.com>
2016-05-19 22:19:27 +0800

    图片居中显示

--- weibo/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
+++ weibo/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
@@ -14,16 +14,47 @@ class PhotoBrowserCell: UICollectionViewCell {
     var imageURL:NSURL?{
         didSet{
             iconView.sd_setImageWithURL(imageURL) { (image, _, _, _) -> Void in
-                let size = self.displaySize(image)
-                self.iconView.frame = CGRect(origin: CGPointZero, size: size)
+//                let size = self.displaySize(image)
+//                self.iconView.frame = CGRect(origin: CGPointZero, size: size)
+                self.setImageViewPostion()
             }
         }
     }
+
+
+    /**
+     调整图片显示的位置
+     */
+    private func setImageViewPostion()
+    {
+        // 1.拿到按照宽高比计算之后的图片大小
+        let size = self.displaySize(iconView.image!)
+        // 2.判断图片的高度, 是否大于屏幕的高度
+        if size.height < UIScreen.mainScreen().bounds.height
+        {
+            // 2.2小于 短图 --> 设置边距, 让图片居中显示
+            iconView.frame = CGRect(origin: CGPointZero, size: size)
+            // 处理居中显示
+            let y = (UIScreen.mainScreen().bounds.height - size.height) * 0.5
+            self.scrollview.contentInset = UIEdgeInsets(top: y, left: 0, bottom: y, right: 0)
+        }else
+        {
+            // 2.1大于 长图 --> y = 0, 设置scrollview的滚动范围为图片的大小
+            iconView.frame = CGRect(origin: CGPointZero, size: size)
+            scrollview.contentSize = size
+        }
+    }
     
-    private func displaySize(image: UIImage) -> CGSize {
+    /**
+     按照图片的宽高比计算图片显示的大小
+     */
+    private func displaySize(image: UIImage) -> CGSize
+    {
+        // 1.拿到图片的宽高比
         let scale = image.size.height / image.size.width
+        // 2.根据宽高比计算高度
         let width = UIScreen.mainScreen().bounds.width
-        let height = width * scale
+        let height =  width * scale
         
         return CGSize(width: width, height: height)
     }
@@ -40,7 +71,7 @@ class PhotoBrowserCell: UICollectionViewCell {
     private func setupUI()
     {
         contentView.addSubview(scrollview)
-        contentView.addSubview(iconView)
+        scrollview.addSubview(iconView)
         
         scrollview.frame = UIScreen.mainScreen().bounds
     }
